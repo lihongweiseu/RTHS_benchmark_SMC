62ae7e184c2356173893331af401629378570918
lihongweiseu <40768158+lihongweiseu@users.noreply.github.com>
2020-05-06 10:44:51 +0800

    Update F2_controller.m

--- F2_controller.m
+++ F2_controller.m
@@ -14,10 +14,10 @@ lambda=150; eta=0.1;
 Para=[b c b*bc c*cc lambda eta];
 
 % Kalman filter design
-[Ax,Bx,Cx,Dx] = tf2ss(1,[1 b c]);
+[Ax,Bx,Cx,Dx] = tf2ss([0 1;1 0],[1 b c]);
 Rx=rms_noise_DT/dt_rths;
-[KESTx,Lx,Px] = kalman(ss(Ax,zeros(2,1),Cx,0),0,Rx,0);
-Axf=Ax-Lx*Cx; Bxf=[Bx-Lx*Dx,Lx]; Cxf=Cx; Dxf=[Dx,0];
+[KESTx,Lx,Px] = kalman(ss(Ax,zeros(2,1),Cx(1,:),0),0,Rx,0);
+Axf=Ax-Lx*Cx(1,:); Bxf=[Bx-Lx*Dx(1),Lx]; Cxf=Cx; Dxf=[Dx,zeros(2,1)];
 xinitial=zeros(2,0);
 
 % Phase lead design
