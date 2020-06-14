3cba576295c9855e4ac48a27bf98ab5f46f71c27
lihongweiseu <40768158+lihongweiseu@users.noreply.github.com>
2020-06-14 09:40:34 +0800

    Update F4_evaluation.m

--- F4_evaluation.m
+++ F4_evaluation.m
@@ -1,98 +1,62 @@
-clc, clear, close all
-
-real_time = 'n';
-eq_intensity = .4;           % EQ Intensity 
-                             %   1: El centro, 2: Kobe, 3: Morgan,  4: Chirp input
-f_ini = 0;                   % Chirp initial freq. [Hz] 
-f_end = 10;                  % Chirp end freq. [Hz]
-
-fs = 4096;              % Sampling frequency [Hz]
-dt_rths = 1/fs;         % Sampling period [sec]
-load_system('vRTHS_MDOF_SimRT.slx')           
-switch lower(real_time)
-    case 'y'
-        set_param('vRTHS_MDOF_SimRT/Real-Time Synchronization','Commented','off')
-        set_param('vRTHS_MDOF_SimRT/Missed Ticks','Commented','off')      
-    case 'n'
-        set_param('vRTHS_MDOF_SimRT/Real-Time Synchronization','Commented','on')
-        set_param('vRTHS_MDOF_SimRT/Missed Ticks','Commented','on')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                          Evaluation Measurement                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% NOTE: This portion of the file should not be modified.
+if ii == 1
+    test{ii,1} = strcat('Nominal');
+else
+    test{ii,1} = strcat('Perturbed_ ',num2str(ii-1));
 end
 
-num_add = 0;
-num_t = 1+num_add;
-E_sw=1; Building_c=4; ii=1;
-
-F1_input_file
-
-sw=0; % 0 means without boundary layer; 1 means with.
-F2_controller
-set_param(bdroot,'SolverType','Fixed-step','Solver','ode4','StopTime','tend','FixedStep','dt_rths')
-sim ('vRTHS_MDOF_SimRT')
-u0=u;
-
-sw=1; % 0 means without boundary layer; 1 means with.
-F2_controller
-sim ('vRTHS_MDOF_SimRT')
-u1=u;
-%%
-figure; set(gcf,'Position',[0 0 900 450]);
-subplot(2,2,1:2);
-plot(t,u0,'b','linewidth',1); hold on;
-plot(t,u1,'--r','linewidth',1); grid on;
-axesH = gca;
-set(axesH,'fontsize',16,'TickLabelInterpreter','latex');
-axesH.XAxis.TickLabelFormat ='\\textbf{%g}';axesH.YAxis.TickLabelFormat ='\\textbf{%g}';
-ylabel('\textbf{Command (m)}','interpreter','latex');
-xlabel('\textbf{Time (s)}','interpreter','latex');
-xlim([0,40]);
-ylim([-240,240]); set(gca,'YTick',-240:120:240);
-legend({'\textbf{Without boundary layer}','\textbf{With boundary layer}'},'interpreter','latex','location','NorthEast');
-subplot(2,2,3);
-plot(t,u0,'b','linewidth',1); hold on;
-plot(t,u1,'--r','linewidth',1); grid on;
-xlim([14,15]);
-subplot(2,2,4);
-plot(t,u0,'b','linewidth',1); hold on;
-plot(t,u1,'--r','linewidth',1); grid on;
-xlim([24,25]);
-
-for i=3:4
-    subplot(2,2,i);
-    axesH = gca;
-    set(axesH,'fontsize',16,'TickLabelInterpreter','latex');
-    axesH.XAxis.TickLabelFormat ='\\textbf{%g}';axesH.YAxis.TickLabelFormat ='\\textbf{%g}';
-    ylabel('\textbf{Command (m)}','interpreter','latex');
-    xlabel('\textbf{Time (s)}','interpreter','latex');
+N = 1:50;
+for jj = N
+    R1 = corr(x(jj:end,1),Num_resp.Data(1:end-jj+1,1));
+    r1(jj) = R1;
+%     R4 = corr(x(jj:end,1),Ref_Resp(1:end-jj+1,1));
+%     r4(jj) = R4;
 end
-ylim([-100,100]); set(gca,'YTick',-100:50:100);
-subplot(2,2,3);
-ylim([-180,180]); set(gca,'YTick',-180:90:180);
 
-% print -depsc F_Command;
+% 1. Tracking time delay from cross correlation
+[Y,I] = max(r1);
+del1 = (N-1)*dt_rths;
+    J1(ii,1) = round(10000*del1(I))/10;
 
-%%
-E=v-vd+lambda*(x-xd);
-figure; set(gcf,'Position',[0 0 900 520]);
-subplot(2,1,1);
-plot(t,E,'b','linewidth',1); grid on;
-subplot(2,1,2);
-plot(t,E,'b','linewidth',1.5); hold on;
-plot(t,Phi,'r','linewidth',1); grid on;
-plot(t,-Phi,'r','linewidth',1);
+% 2. Normalized root mean square of experimental tracking error
+    J2(ii,1) = round(10000*std(x(:,1)-Num_resp.Data(:,1))/std(Num_resp.Data(:,1)))/100;
+% 3. Tracking peak error
+    J3(ii,1) = round(10000*max(abs(x(:,1)-Num_resp.Data(:,1)))/max(abs(Num_resp.Data(:,1))))/100;
+% 4. Displacement normalized root mean square error (1st floor)
+    J4(ii,1) = round(10000*(std(x(:,1)-Ref_Resp(:,1))/std(Ref_Resp(:,1))))/100;
+% 5. Displacement peak error (1st floor)
+    J5(ii,1) = round(10000*max(abs(x(:,1)-Ref_Resp(:,1)))/max(abs(Ref_Resp(:,1))))/100;
+% 6. Displacement normalized root mean square error (2nd floor)
+    J6(ii,1) = round(10000*(std(Num_resp.Data(:,2)-Ref_Resp(:,2))/std(Ref_Resp(:,2))))/100;
+% 7. Displacement normalized root mean square error (3nd floor)
+    J7(ii,1) = round(10000*(std(Num_resp.Data(:,3)-Ref_Resp(:,3))/std(Ref_Resp(:,3))))/100;
+% 8. Displacement peak error   (2nd floor)
+    J8(ii,1) = round(10000*max(abs(Num_resp.Data(:,2)-Ref_Resp(:,2)))/max(abs(Ref_Resp(:,2))))/100;
+% 9. Displacement peak error   (3nd floor)
+    J9(ii,1) = round(10000*max(abs(Num_resp.Data(:,3)-Ref_Resp(:,3)))/max(abs(Ref_Resp(:,3))))/100;
+    
+eval_crit = [J1(ii,1) J2(ii,1) J3(ii,1) J4(ii,1) J5(ii,1) J6(ii,1) J7(ii,1) J8(ii,1) J9(ii,1)];
 
-for i=1:2
-    subplot(2,1,i);
-    xlim([0,40]);
-    axesH = gca;
-    set(axesH,'fontsize',16,'TickLabelInterpreter','latex');
-    axesH.XAxis.TickLabelFormat ='\\textbf{%g}';axesH.YAxis.TickLabelFormat ='\\textbf{%g}';
-    ylabel('\textbf{Compact error (m/s)}','interpreter','latex');
+%% NOTE: This portion of the file should not be modified.
+
+time = clock;
+time_test = strcat(num2str(time(1)),'_',... % Returns year 
+    num2str(time(2)),'_',... % Returns month 
+    num2str(time(3)),'_',... % Returns day 
+    num2str(time(4)),'_',... % returns hour 
+    num2str(time(5)),'_',... % returns min 
+    num2str(fix(time(6)))) ; 
+
+if ii == 1
+    save(strcat('.\Results\Data_',time_test,'_Nom_'),'t', 'Ref_Resp','Num_resp','X_m','eval_crit');
+else
+    save(strcat('.\Results\Data_',time_test,'_Pert_',num2str(ii-1)),'t', 'Ref_Resp','Num_resp','X_m','eval_crit');
 end
-xlabel('\textbf{Time (s)}','interpreter','latex');
-% ylim([-0.06,0.06]); set(gca,'YTick',-0.06:0.03:0.06);
-legend({'\textbf{{\it E}-trajectory}','\textbf{Boundary layer}'},'interpreter','latex','location','NorthEast');
-subplot(2,1,1);
-ylim([-6e-16,6e-16]); set(gca,'YTick',-6e-16:3e-16:6e-16);
-legend({'\textbf{{\it E}-trajectory}'},'interpreter','latex','location','NorthEast');
-% title('\textbf{{\it E}-trajectory and time-varying boundary layer}','fontsize',14,'interpreter','latex');
-% print -depsc F_E_trajectory;
+
+
+% if max(abs(Force_act.Data)) >= 8900
+%     clc, error('ERROR. The actuator capacity was exceeded.')
+% end
